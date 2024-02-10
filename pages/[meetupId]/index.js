987e833acaf002af97afdf46a9fa2d2a8eb009f8
codingpartnerNB <nehab5314@gmail.com>
2024-02-10 16:24:38 +0530

    Making the Details page work properly

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,51 +1,69 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="A First Meetup"
-      address="Some address 3, 230 Some City"
-      description="This is our first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 
 export async function getStaticPaths(){
+    const client = await MongoClient.connect('mongodb+srv://nehab:T9DcXVDbTd82KCTP@cluster0.q4j14gt.mongodb.net/meetups');
+    const db = client.db();
+    const meetupsCollection = db.collection('meetupsdb');
+    
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray(); //first argument is filter criteria and second is which field we want to extract
+    client.close();
+
     return {
         fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1',
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2',
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm3',
-                }
-            }
-        ]
+        paths: meetups.map(meetup=> ({params: {meetupId: meetup._id.toString()}}))
+        // paths: [
+        //     {
+        //         params: {
+        //             meetupId: 'm1',
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm2',
+        //         }
+        //     },
+        //     {
+        //         params: {
+        //             meetupId: 'm3',
+        //         }
+        //     }
+        // ]
     }
 }
 
 export async function getStaticProps(context){
     const meetupId = context.params.meetupId;
-    console.log(meetupId);
+
+    const client = await MongoClient.connect('mongodb+srv://nehab:T9DcXVDbTd82KCTP@cluster0.q4j14gt.mongodb.net/meetups');
+    const db = client.db();
+    const meetupsCollection = db.collection('meetupsdb');
+    
+    const selectedMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupId)});
+    client.close();
+
+    //image url = https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg
+    
     return {
         props: {
             meetupData: {
-                id: meetupId,
-                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-                title: "A First Meetup",
-                address: "Some address 3, 230 Some City",
-                description: "This is our first meetup"
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                image: selectedMeetup.image,
+                address: selectedMeetup.address,
+                description: selectedMeetup.description
             }
         }
     }
